25379de8d72426d3e05dad2f15969763bcb503a2
lucca freitas <luccadefreitasfaria@hotmail.com>
2024-06-12 18:42:55 -0300

    adicionado botao e salvando colaborador

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
+
+// const [colaboradores, setColaboradores] = useState([])
+
+const aoNovoColaboradorAdicionado = (colaborador) => {
+  console.log(colaborador)
+  // setColaboradores([...colaboradores, colaborador])
+}
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
     </div>
   );
 }
