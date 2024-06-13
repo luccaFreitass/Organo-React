3883c9332be38e82a93f3a5b2c62047110e64c2c
lucca freitas <luccadefreitasfaria@hotmail.com>
2024-06-13 16:10:18 -0300

    removendo duplicação de codigo

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -6,15 +6,6 @@ import './Formulario.css'
 
 const Formulario = (props) => {
 
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'Devops',
-        'UX e Design',
-        'Mobile',
-        'Inovação e Gestão'
-    ]
 
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
@@ -58,7 +49,7 @@ const Formulario = (props) => {
                 <ListaSuspensa
                     obrigatorio={true}
                     label="Time"
-                    itens={times}
+                    itens={props.times}
                     valor={time}
                     aoAlterado={valor => setTime(valor)}
 
