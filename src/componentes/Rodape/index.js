df0983837e53c76c45e9bf81d8162cadff578187
lucca freitas <luccadefreitasfaria@hotmail.com>
2024-06-17 20:15:39 -0300

    adicionando rodape e escondendo componentes vazios

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,17 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (
+        <footer className='rodape'>
+            <div>
+                <img src="/imagens/fb.png" alt="Logo Facebook" />
+                <img src="/imagens/ig.png" alt="Logo instagram" />
+                <img src="/imagens/tw.png" alt="Logo twitter" />
+            </div>
+            <img src="/imagens/logo.png" alt="Logo Facebook" />
+            <p>Desenvolvido por Lucca de Freitas</p>
+        </footer>
+    )
+}
+
+export default Rodape
